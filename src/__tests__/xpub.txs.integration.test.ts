3456acba34e9c466362bb65d3d33cd37039ee2b6
machard <matthieu__@hotmail.fr>
2021-07-12 15:24:26 +0200

    fee per bytes

--- src/__tests__/xpub.txs.integration.test.ts
+++ src/__tests__/xpub.txs.integration.test.ts
@@ -63,7 +63,7 @@ describeToUse('testing xpub legacy transactions', () => {
     }
 
     // time for explorer to sync
-    await sleep(20000);
+    await sleep(30000);
 
     try {
       await xpubs[0].xpub.sync();
@@ -71,7 +71,7 @@ describeToUse('testing xpub legacy transactions', () => {
       // eslint-disable-next-line no-console
       console.log('praline explorer setup error', e);
     }
-  }, 30000);
+  }, 70000);
 
   it('should be setup correctly', async () => {
     const balance1 = await xpubs[0].xpub.getXpubBalance();
@@ -79,6 +79,7 @@ describeToUse('testing xpub legacy transactions', () => {
     expect(balance1.toNumber()).toEqual(5700000000);
   });
 
+  let expectedFee1: number;
   it('should send a 1 btc tx to xpubs[1].xpub', async () => {
     const { address } = await xpubs[1].xpub.getNewAddress(0, 0);
     const { address: change } = await xpubs[0].xpub.getNewAddress(1, 0);
@@ -90,7 +91,7 @@ describeToUse('testing xpub legacy transactions', () => {
     const { inputs, associatedDerivations, outputs } = await xpubs[0].xpub.buildTx(
       address,
       new BigNumber(100000000),
-      500,
+      100,
       change,
       utxoPickingStrategy
     );
@@ -136,15 +137,18 @@ describeToUse('testing xpub legacy transactions', () => {
     }
 
     // time for explorer to sync
-    await sleep(20000);
+    await sleep(30000);
 
     await xpubs[0].xpub.sync();
     await xpubs[1].xpub.sync();
 
-    expect((await xpubs[0].xpub.getXpubBalance()).toNumber()).toEqual(5700000000 - 100000000 - 500);
-    expect((await xpubs[1].xpub.getXpubBalance()).toNumber()).toEqual(100000000);
-  }, 30000);
+    expectedFee1 = 10 * 100 + inputs.length * 100 * 180 + outputs.length * 34 * 100;
 
+    expect((await xpubs[0].xpub.getXpubBalance()).toNumber()).toEqual(5700000000 - 100000000 - expectedFee1);
+    expect((await xpubs[1].xpub.getXpubBalance()).toNumber()).toEqual(100000000);
+  }, 70000);
+
+  let expectedFee2: number;
   it('should send a 1 btc tx to xpubs[1].xpub and handle output splitting', async () => {
     const { address } = await xpubs[1].xpub.getNewAddress(0, 0);
     const { address: change } = await xpubs[0].xpub.getNewAddress(1, 0);
@@ -153,11 +157,11 @@ describeToUse('testing xpub legacy transactions', () => {
 
     const utxoPickingStrategy = new Merge();
 
-    xpubs[0].xpub.OUTPUT_VALUE_MAX = 60000000;
+    xpubs[0].xpub.OUTPUT_VALUE_MAX = 70000000;
     const { inputs, associatedDerivations, outputs } = await xpubs[0].xpub.buildTx(
       address,
       new BigNumber(100000000),
-      500,
+      100,
       change,
       utxoPickingStrategy
     );
@@ -203,12 +207,16 @@ describeToUse('testing xpub legacy transactions', () => {
     }
 
     // time for explorer to sync
-    await sleep(20000);
+    await sleep(30000);
 
     await xpubs[0].xpub.sync();
     await xpubs[1].xpub.sync();
 
-    expect((await xpubs[0].xpub.getXpubBalance()).toNumber()).toEqual(5700000000 - 100000000 - 500 - 100000000 - 500);
+    expectedFee2 = 10 * 100 + inputs.length * 100 * 180 + outputs.length * 34 * 100;
+
+    expect((await xpubs[0].xpub.getXpubBalance()).toNumber()).toEqual(
+      5700000000 - 100000000 - expectedFee1 - 100000000 - expectedFee2
+    );
     expect((await xpubs[1].xpub.getXpubBalance()).toNumber()).toEqual(200000000);
-  }, 30000);
+  }, 70000);
 });
