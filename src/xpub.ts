6e9543e89c9ed1d909f2a7051123ab26c16bed67
machard <matthieu__@hotmail.fr>
2021-07-27 11:03:38 +0200

    expose sequence

--- src/xpub.ts
+++ src/xpub.ts
@@ -209,13 +209,14 @@ class Xpub extends EventEmitter {
     return address;
   }
 
-  async buildTx(
-    destAddress: string,
-    amount: BigNumber,
-    feePerByte: number,
-    changeAddress: string,
-    utxoPickingStrategy: IPickingStrategy
-  ) {
+  async buildTx(params: {
+    destAddress: string;
+    amount: BigNumber;
+    feePerByte: number;
+    changeAddress: string;
+    utxoPickingStrategy: IPickingStrategy;
+    sequence?: number;
+  }) {
     await this.whenSynced('all');
 
     const outputs = [];
@@ -225,8 +226,8 @@ class Xpub extends EventEmitter {
     // we use to serialize output only take js number in params
     // that are actually even more restricted
     const desiredOutputLeftToFit = {
-      script: this.crypto.toOutputScript(destAddress),
-      value: amount,
+      script: this.crypto.toOutputScript(params.destAddress),
+      value: params.amount,
     };
 
     while (desiredOutputLeftToFit.value.gt(this.OUTPUT_VALUE_MAX)) {
@@ -246,7 +247,12 @@ class Xpub extends EventEmitter {
       totalValue: total,
       unspentUtxos: unspentUtxoSelected,
       fee,
-    } = await utxoPickingStrategy.selectUnspentUtxosToUse(this, amount, feePerByte, outputs.length);
+    } = await params.utxoPickingStrategy.selectUnspentUtxosToUse(
+      this,
+      params.amount,
+      params.feePerByte,
+      outputs.length
+    );
 
     const txHexs = await Promise.all(
       unspentUtxoSelected.map((unspentUtxo) => this.explorer.getTxHex(unspentUtxo.output_hash))
@@ -256,15 +262,20 @@ class Xpub extends EventEmitter {
     );
 
     // formatting approx the ledger way; ledger for the win
-    const inputs: [string, number][] = unspentUtxoSelected.map((utxo, index) => [txHexs[index], utxo.output_index]);
+    const inputs: [string, number, null, number | null][] = unspentUtxoSelected.map((utxo, index) => [
+      txHexs[index],
+      utxo.output_index,
+      null,
+      Number.isInteger(params.sequence) ? params.sequence : null,
+    ]);
     const associatedDerivations: [number, number][] = unspentUtxoSelected.map((utxo, index) => [
       txs[index].account,
       txs[index].index,
     ]);
 
     outputs.push({
-      script: this.crypto.toOutputScript(changeAddress),
-      value: total.minus(amount).minus(fee),
+      script: this.crypto.toOutputScript(params.changeAddress),
+      value: total.minus(params.amount).minus(fee),
     });
 
     return {
