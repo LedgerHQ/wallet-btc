a542d1ae86530be6a4cfaded20558bac7dd29f6f
Hakim <59644786+haammar-ledger@users.noreply.github.com>
2021-08-23 13:46:45 +0200

    Expose a getCurrentBlock API in the explorer (#32)

--- src/explorer/types.ts
+++ src/explorer/types.ts
@@ -6,6 +6,7 @@ export interface IExplorer {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   broadcast(tx: string): Promise<any>;
   getTxHex(txId: string): Promise<string>;
+  getCurrentBlock(): Promise<Block | null>;
   getBlockByHeight(height: number): Promise<Block | null>;
   getPendings(address: Address, nbMax?: number): Promise<TX[]>;
   getAddressTxsSinceLastTxBlock(batchSize: number, address: Address, lastTx: TX | undefined): Promise<TX[]>;
