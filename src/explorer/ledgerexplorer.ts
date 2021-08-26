f50b92750a8cdb2b03a560a729c5fc34f0fb09b7
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-08-26 15:40:41 +0200

    type safety

--- src/explorer/ledgerexplorer.ts
+++ src/explorer/ledgerexplorer.ts
@@ -231,7 +231,7 @@ class LedgerExplorer extends EventEmitter implements IExplorer {
       output.block_height = tx.block ? tx.block.height : null;
       // Definition of replaceable, per the standard: https://github.com/bitcoin/bips/blob/61ccc84930051e5b4a99926510d0db4a8475a4e6/bip-0125.mediawiki#summary
       // eslint-disable-next-line @typescript-eslint/camelcase,no-param-reassign
-      output.rbf = tx.inputs[0].sequence < 0xffffffff;
+      output.rbf = tx.inputs[0]?.sequence && tx.inputs[0].sequence < 0xffffffff;
     });
   }
 
