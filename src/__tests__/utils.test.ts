79d28810dd535a22ca645e13a67ed10701cc2a85
Kalle Rosenbaum <kalle@rosenbaum.se>
2021-09-14 11:39:47 +0200

    Test validation of altcoin addresses
    Ignored for now until those validations are implemented

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -2,7 +2,7 @@ import { bech32m, bech32, BechLib } from 'bech32';
 import * as utils from '../utils';
 import { Currency } from '../crypto/types';
 
-function checkValidAddresses(addresses: string[], currency: Currency, expectedValid: boolean) {
+function validateAddrs(addresses: string[], currency: Currency, expectedValid: boolean) {
   addresses.forEach((address: string) => {
     expect(utils.isValidAddress(address, currency)).toEqual(expectedValid);
   });
@@ -53,7 +53,7 @@ describe('Unit tests for various utils functions', () => {
       'bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0',
       v1addrEncodedWithBase32m,
     ];
-    checkValidAddresses(validMainnetAddresses, 'bitcoin', true);
+    validateAddrs(validMainnetAddresses, 'bitcoin', true);
 
     const validTestnetAddresses = [
       // bech32
@@ -62,7 +62,7 @@ describe('Unit tests for various utils functions', () => {
       'tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy',
       'tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c',
     ];
-    checkValidAddresses(validTestnetAddresses, 'bitcoin_testnet', true);
+    validateAddrs(validTestnetAddresses, 'bitcoin_testnet', true);
 
     const invalidMainnetAddresses = [
       'bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqh2y7hd', // Invalid checksum (Bech32 instead of Bech32m)
@@ -79,7 +79,7 @@ describe('Unit tests for various utils functions', () => {
       v0addrEncodedWithBase32m, // Version 0 address must be encoded with bech32
       v1addrEncodedWithBase32, // Version 1 address must be encoded with bech32m
     ];
-    checkValidAddresses(invalidMainnetAddresses, 'bitcoin', false);
+    validateAddrs(invalidMainnetAddresses, 'bitcoin', false);
 
     const invalidTestnetAddresses = [
       'tc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq5zuyut', // Invalid human-readable part
@@ -89,11 +89,57 @@ describe('Unit tests for various utils functions', () => {
       'tb1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vpggkg4j', // Non-zero padding in 8-to-5 conversion
       'bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y', // Mainnet addr used on testnet
     ];
-    checkValidAddresses(invalidTestnetAddresses, 'bitcoin_testnet', false);
+    validateAddrs(invalidTestnetAddresses, 'bitcoin_testnet', false);
   });
 
   it('isValidAddress should default old validation if currency not provided', () => {
     expect(utils.isValidAddress('tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7')).toBeTruthy();
     expect(utils.isValidAddress('tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k8')).toBeFalsy();
+  });
+
+  /*
+  TODO: Enable this test and make it work
+   */
+  it.skip('Test altcoins', () => {
+    validateAddrs(
+      ['1BpEi6DfDAUFd7GtittLSdBeYJvcoaVggu', 'qqmyc72pkyx8c0ppgeuummq6clzverhxnsk3qh6jcf'],
+      'bitcoin_cash',
+      true
+    );
+    validateAddrs(['qqmyc72pkyx8c0ppgeuummq6clzverhxnsk3qh6jc1'], 'bitcoin_cash', false);
+
+    validateAddrs(['qzl0x0982hy9xrh99wdnejx4eecdn02jv58as5p595'], 'bitcoin_cash', true);
+    validateAddrs(['ltc1q3e4eh3lldvx97zg6d74x4ns6v5a4j4hwwqycwv'], 'litecoin', true);
+    validateAddrs(['Xh13o3cWixDBYJMGuJmFX17TZb9guXcBik'], 'dash', true);
+    validateAddrs(['MFYvHZcZ35typC4k2XyvRVooCDZxnoDS4B'], 'qtum', true);
+    validateAddrs(['t1b1Rbw2shhJkP6MCnCyxCPuyFedHrwKty8'], 'zcash', true);
+    validateAddrs(['AYRf8r4SJhmfaEwmWvY8ujmrepbrWyenFr'], 'bitcoin_gold', true);
+    validateAddrs(['D8cMCRimfjwQ9E8jJvgUswt18WnZbCUAaW'], 'dogecoin', true);
+    validateAddrs(['dgb1q7zjgqa23xzf602ljfrc94248a9u27xml08nhct'], 'digibyte', true);
+    validateAddrs(['REnTkuvjsmfshAZ3vukYfuS3ZGvfUatqFc'], 'komodo', true);
+    validateAddrs(['DQ7F2iTQn6kUpyvietinaC3Cxje2m1ULjS'], 'pivx', true);
+    validateAddrs(['zndVnjbrzRxvhPGRea4jgsfSWE1hXFja1dc'], 'zencash', true);
+    validateAddrs(['3LQWwDEpgjjQokDusDXtoF9c8on8zr54fT'], 'vertcoin', true);
+    validateAddrs(['PCYt39DMYuFqUYkP8w6ubZXJbTTL4c9KAV'], 'peercoin', true);
+    validateAddrs(['ETx91CT52eBFYWbe4Yht5BpZNigWg2EkqJ'], 'viacoin', true);
+    validateAddrs(['7i1KkJHUjfw2MrbtXK5DQkhz7zd36st9GR'], 'stakenet', true);
+    validateAddrs(['S6NMcEfYbavHrP3Uo1wbEUvKhAbKeMuga8'], 'stealthcoin', true);
+
+    validateAddrs(['qzl0x0982hy9xrh99wdnejx4eecdn02jv58as5p599'], 'bitcoin_cash', false);
+    validateAddrs(['ltc1q3e4eh3lldvx97zg6d74x4ns6v5a4j4hwwqycww'], 'litecoin', false);
+    validateAddrs(['Xh13o3cWixDBYJMGuJmFX17TZb9guXcBii'], 'dash', false);
+    validateAddrs(['MFYvHZcZ35typC4k2XyvRVooCDZxnoDS44'], 'qtum', false);
+    validateAddrs(['t1b1Rbw2shhJkP6MCnCyxCPuyFedHrwKtyy'], 'zcash', false);
+    validateAddrs(['AYRf8r4SJhmfaEwmWvY8ujmrepbrWyenFF'], 'bitcoin_gold', false);
+    validateAddrs(['D8cMCRimfjwQ9E8jJvgUswt18WnZbCUAaa'], 'dogecoin', false);
+    validateAddrs(['dgb1q7zjgqa23xzf602ljfrc94248a9u27xml08nhcc'], 'digibyte', false);
+    validateAddrs(['REnTkuvjsmfshAZ3vukYfuS3ZGvfUatqFF'], 'komodo', false);
+    validateAddrs(['DQ7F2iTQn6kUpyvietinaC3Cxje2m1ULjj'], 'pivx', false);
+    validateAddrs(['zndVnjbrzRxvhPGRea4jgsfSWE1hXFja1dd'], 'zencash', false);
+    validateAddrs(['3LQWwDEpgjjQokDusDXtoF9c8on8zr54ff'], 'vertcoin', false);
+    validateAddrs(['PCYt39DMYuFqUYkP8w6ubZXJbTTL4c9KAA'], 'peercoin', false);
+    validateAddrs(['ETx91CT52eBFYWbe4Yht5BpZNigWg2Ekqq'], 'viacoin', false);
+    validateAddrs(['7i1KkJHUjfw2MrbtXK5DQkhz7zd36st9GG'], 'stakenet', false);
+    validateAddrs(['S6NMcEfYbavHrP3Uo1wbEUvKhAbKeMugaa'], 'stealthcoin', false);
   });
 });
